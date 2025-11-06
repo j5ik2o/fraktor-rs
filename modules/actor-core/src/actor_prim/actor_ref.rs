d8a0f814427c4c7c3232de649489f3ec85817e07
Junichi Kato <j5ik2o@gmail.com>
2025-11-06 16:30:28 +0900

    feat: add setup behavior for deferred actor initialization and implement worker behavior

--- modules/actor-core/src/actor_prim/actor_ref.rs
+++ modules/actor-core/src/actor_prim/actor_ref.rs
@@ -1,13 +1,13 @@
 //! Actor reference handle module aggregation.
 
-mod base;
 mod actor_ref_sender;
 mod ask_reply_sender;
+mod base;
 mod null_sender;
 
-pub use base::{ActorRef, ActorRefGeneric};
 pub use actor_ref_sender::ActorRefSender;
 pub use ask_reply_sender::AskReplySender;
+pub use base::{ActorRef, ActorRefGeneric};
 pub use null_sender::NullSender;
 
 #[cfg(test)]
