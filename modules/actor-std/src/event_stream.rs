ca9bda26a80798b802ead838f4392348ed33227f
Junichi Kato <j5ik2o@gmail.com>
2025-11-06 22:40:05 +0900

    feat: expose subscriber_adapter module and adjust visibility of EventStreamSubscriberAdapter

--- modules/actor-std/src/event_stream.rs
+++ modules/actor-std/src/event_stream.rs
@@ -1,20 +1,7 @@
 mod subscriber;
-mod subscriber_adapter;
+pub mod subscriber_adapter;
 mod types;
 
-use cellactor_actor_core_rs::event_stream::EventStreamSubscriber as CoreEventStreamSubscriber;
-use cellactor_utils_core_rs::sync::ArcShared;
-use cellactor_utils_std_rs::runtime_toolbox::StdToolbox;
 pub use subscriber::EventStreamSubscriber;
 pub use types::*;
 
-use self::subscriber_adapter::EventStreamSubscriberAdapter;
-use crate::system::ActorSystem;
-
-/// Subscribes an observer implemented using [`EventStreamSubscriber`].
-#[must_use]
-pub fn subscribe(system: &ActorSystem, subscriber: &ArcShared<dyn EventStreamSubscriber>) -> EventStreamSubscription {
-  let adapter: ArcShared<dyn CoreEventStreamSubscriber<StdToolbox>> =
-    ArcShared::new(EventStreamSubscriberAdapter::new(subscriber.clone()));
-  system.as_core().subscribe_event_stream(&adapter)
-}
