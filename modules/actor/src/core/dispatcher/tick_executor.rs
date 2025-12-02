6e3b5e475b1965cbe69c3978390373136a1f06d2
Junichi Kato <j5ik2o@gmail.com>
2025-12-03 01:21:15 +0900

    refactor: remove interior mutability from tick executor components

--- modules/actor/src/core/dispatcher/tick_executor.rs
+++ modules/actor/src/core/dispatcher/tick_executor.rs
@@ -1,6 +1,6 @@
 use alloc::collections::VecDeque;
 
-use fraktor_utils_rs::core::runtime_toolbox::{NoStdMutex, NoStdToolbox, RuntimeToolbox};
+use fraktor_utils_rs::core::runtime_toolbox::{NoStdToolbox, RuntimeToolbox};
 
 use super::{
   dispatch_error::DispatchError, dispatch_executor::DispatchExecutor, dispatch_shared::DispatchSharedGeneric,
@@ -8,34 +8,27 @@ use super::{
 
 /// Executor that queues dispatcher batches until `tick` is invoked.
 pub struct TickExecutorGeneric<TB: RuntimeToolbox + 'static> {
-  queue: NoStdMutex<VecDeque<DispatchSharedGeneric<TB>>>,
+  queue: VecDeque<DispatchSharedGeneric<TB>>,
 }
 
 impl<TB: RuntimeToolbox + 'static> TickExecutorGeneric<TB> {
   /// Creates an empty tick-driven executor.
   #[must_use]
   pub const fn new() -> Self {
-    Self { queue: NoStdMutex::new(VecDeque::new()) }
+    Self { queue: VecDeque::new() }
   }
 
   /// Drains all pending dispatcher batches.
   pub fn tick(&mut self) {
-    loop {
-      let dispatcher = {
-        let mut queue = self.queue.lock();
-        queue.pop_front()
-      };
-      match dispatcher {
-        | Some(task) => task.drive(),
-        | None => break,
-      }
+    while let Some(task) = self.queue.pop_front() {
+      task.drive();
     }
   }
 
   /// Returns the number of queued tasks (testing helper).
   #[must_use]
   pub fn pending_tasks(&self) -> usize {
-    self.queue.lock().len()
+    self.queue.len()
   }
 }
 
@@ -56,7 +49,7 @@ where
   TB: RuntimeToolbox + Send + Sync + 'static,
 {
   fn execute(&mut self, dispatcher: DispatchSharedGeneric<TB>) -> Result<(), DispatchError> {
-    self.queue.lock().push_back(dispatcher);
+    self.queue.push_back(dispatcher);
     Ok(())
   }
 }
