061b38b1b8709a64aca6c9a26948e52e9dc4c342
Junichi Kato <j5ik2o@gmail.com>
2026-01-03 22:30:47 +0900

    refactor: rename dead_letter_impl to base and update references

--- modules/actor/src/core/dead_letter.rs
+++ modules/actor/src/core/dead_letter.rs
@@ -3,12 +3,12 @@
 //! This module contains undeliverable message handling.
 
 mod dead_letter_entry;
-mod dead_letter_impl;
+mod base;
 mod dead_letter_reason;
 mod dead_letter_shared;
 
 pub use dead_letter_entry::{DeadLetterEntry, DeadLetterEntryGeneric};
-pub use dead_letter_impl::{DeadLetter, DeadLetterGeneric};
+pub use base::{DeadLetter, DeadLetterGeneric};
 pub use dead_letter_reason::DeadLetterReason;
 pub use dead_letter_shared::{DeadLetterShared, DeadLetterSharedGeneric};
 
