2011a1898b4833a25fc6e5e7b63fe3f43f556c17
Junichi Kato <j5ik2o@gmail.com>
2026-04-05 19:42:45 +0900

    refactor(std): reorder `time` module declaration for consistent visibility

--- modules/actor-adaptor/src/std.rs
+++ modules/actor-adaptor/src/std.rs
@@ -4,8 +4,8 @@ pub mod dispatch;
 pub mod event;
 /// Scheduler bindings for the standard toolbox.
 mod scheduler;
-/// Time bindings for the standard toolbox.
-pub mod time;
 
 #[cfg(feature = "tokio-executor")]
 pub use scheduler::{default_tick_driver_config, tick_driver_config_with_resolution};
+/// Time bindings for the standard toolbox.
+pub mod time;
