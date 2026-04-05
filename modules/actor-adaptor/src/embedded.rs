eaceb3bd94321fd4e8ee645f7bccfb821ccfdfcc
Junichi Kato <j5ik2o@gmail.com>
2026-04-05 09:56:47 +0900

    feat: add module structure for embedded runtime adaptors

---
+++ modules/actor-adaptor/src/embedded.rs
@@ -0,0 +1,3 @@
+//! Reserved for future embedded runtime adaptors.
+
+mod base;
--- modules/actor-adaptor/src/lib.rs
+++ modules/actor-adaptor/src/lib.rs
@@ -7,5 +7,7 @@
 
 extern crate alloc;
 
+/// Embedded runtime adaptors.
+pub mod embedded;
 /// Standard library runtime bindings and utilities.
 pub mod std;
