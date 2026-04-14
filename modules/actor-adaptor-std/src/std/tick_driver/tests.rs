e833e36c79551be40e13d9d652a91c5b78ccf7ab
Junichi Kato <j5ik2o@gmail.com>
2026-04-14 22:36:30 +0900

    test(actor-adaptor-std): add shutdown completion assertion with timeout for StdTickDriver tests

--- modules/actor-adaptor-std/src/std/tick_driver/tests.rs
+++ modules/actor-adaptor-std/src/std/tick_driver/tests.rs
@@ -1,6 +1,7 @@
 extern crate std;
 
 use core::time::Duration;
+use std::sync::mpsc;
 
 use fraktor_actor_core_rs::core::kernel::{
   actor::{
@@ -11,6 +12,7 @@ use fraktor_actor_core_rs::core::kernel::{
 };
 
 use super::StdTickDriver;
+use crate::std::StdBlocker;
 
 struct GuardianActor;
 
@@ -27,17 +29,33 @@ fn build_system_with_driver(driver: StdTickDriver) -> ActorSystem {
   ActorSystem::create_with_config(&props, config).expect("system should build")
 }
 
+fn assert_shutdown_completes(system: ActorSystem, operation: &str) {
+  let termination = system.when_terminated();
+  system.terminate().expect("terminate");
+  termination.wait_blocking(&StdBlocker::new());
+
+  let (done_tx, done_rx) = mpsc::channel();
+  std::thread::spawn(move || {
+    drop(system);
+    done_tx.send(()).expect("send shutdown completion");
+  });
+
+  done_rx
+    .recv_timeout(Duration::from_secs(5))
+    .unwrap_or_else(|_| panic!("{operation}: timed out waiting for StdTickDriver shutdown completion"));
+}
+
 #[test]
 fn std_tick_driver_boots_actor_system() {
   let driver = StdTickDriver::new(Duration::from_millis(10));
   let system = build_system_with_driver(driver);
-  system.terminate().expect("terminate");
+  assert_shutdown_completes(system, "StdTickDriver::new");
 }
 
 #[test]
 fn std_tick_driver_default_boots_actor_system() {
   let system = build_system_with_driver(StdTickDriver::default());
-  system.terminate().expect("terminate");
+  assert_shutdown_completes(system, "StdTickDriver::default");
 }
 
 #[cfg(feature = "tokio-executor")]
